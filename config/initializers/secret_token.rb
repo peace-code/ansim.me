39d3c56aaabd754b305c5e68e36980eebb28b0a9
KWEON Ohyeon <rest515@gmail.com>
2012-08-28 10:48:27 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Goodclinic::Application.config.secret_token = 'd9bc6e882b47f7a10702df3075a48e06463bdb946610a0bb78d605b64b51594b6521409f9a57c908bf5b830ec85e73ee3f16c06c2cdd8bcd41a8e41335bed96e'

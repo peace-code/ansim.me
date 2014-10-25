1349e9583fdfa0091fc28b7fc24c0959abee22ac
wagurano <wagurano@gmail.com>
2014-10-25 15:26:30 +0900

    list pharm and aed
    extend pharmarcy and aed

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,15 +1,22 @@
 class PlacesController < ApplicationController
   def index
-    type = params[:type]
+    @type = params[:type]
 
-    north_east = params[:north_east].split(',').map(&:to_f) if params[:north_east]
-    south_west = params[:south_west].split(',').map(&:to_f) if params[:south_west]
+    @north_east = params[:north_east].split(',').map(&:to_f) if params[:north_east]
+    @south_west = params[:south_west].split(',').map(&:to_f) if params[:south_west]
 
-    if north_east and south_west
-      places = Place.type(type).where(coordinates: {'$within' => {'$box' => [south_west, north_east]}}).limit(300)
+    if @north_east and @south_west
+      @places = Place.type(@type).where(coordinates: {'$within' => {'$box' => [@south_west, @north_east]}}).limit(300)
+    else
+      @search_name = params[:search_name]
+      @places = @search_name ? Place.type(@type).where(name: /#{@search_name}/) : Place.type(@type)
+      @places = @places.page(params[:page])
     end
 
-    render json: places
+    respond_to do |format|
+      format.html
+      format.json { render json: @places }
+    end
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   get "maps/hospitals", as: 'hospital_map'
   get "maps/aeds", as: 'aed_map'
+  get "maps/pharms", as: 'pharm_map'
+
 
   get "pages/comments"
   get "pages/about"
